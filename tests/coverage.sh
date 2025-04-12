619d9179b1f058ea90fa66d757f4e4598a78b81f
sudacode <suda@sudacode.com>
2025-04-11 21:49:55 -0700

    remove tests

--- tests/coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-coverage run -m pytest
-coverage html
-coverage report -m
--- tests/fixtures/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Test fixtures package."""
-
-# This file can be empty, it's just to make the directory a proper package
